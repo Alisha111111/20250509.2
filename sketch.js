56a33c28b749f052ee40e410a9187d88ae1a6dc2
Alisha111111 <413730218@o365.tku.edu.tw>
2025-05-09 11:29:00 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,9 +9,9 @@ const circleSize = 100;
 
 function setup() {
   createCanvas(640, 480);
-  video = createCapture(VIDEO, function () {
-    console.log("Video capture started");
-  });
+
+  // 初始化攝影機
+  video = createCapture(VIDEO);
   video.size(width, height);
   video.hide(); // 隱藏原始影像，使用 p5 畫布顯示
 
@@ -27,6 +27,7 @@ function setup() {
   circleX = width / 2;
   circleY = height / 2;
 }
+
 function modelReady() {
   console.log("Handpose model loaded!");
 }
@@ -50,9 +51,6 @@ function drawKeypoints() {
     const hand = predictions[i];
     const keypoints = hand.landmarks;
 
-    // 繪製手部關鍵點的連接線
-    drawHandLines(keypoints);
-
     // 繪製食指指尖的圓
     fill(0, 255, 0);
     noStroke();
@@ -62,26 +60,6 @@ function drawKeypoints() {
     if (dist(keypoints[8][0], keypoints[8][1], circleX, circleY) < circleSize / 2) {
       circleX = keypoints[8][0];
       circleY = keypoints[8][1];
-    }
-  }
-}
-
-function drawHandLines(keypoints) {
-  // 繪製手指的連接線
-  const fingers = [
-    [0, 1, 2, 3, 4],    // 大拇指
-    [5, 6, 7, 8],       // 食指
-    [9, 10, 11, 12],    // 中指
-    [13, 14, 15, 16],   // 無名指
-    [17, 18, 19, 20]    // 小指
-  ];
-
-  for (let finger of fingers) {
-    for (let j = 0; j < finger.length - 1; j++) {
-      line(
-        keypoints[finger[j]][0], keypoints[finger[j]][1],
-        keypoints[finger[j + 1]][0], keypoints[finger[j + 1]][1]
-      );
     }
   }
 }
